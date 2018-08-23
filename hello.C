c2f4a3b60fb01041a66bebaa2580b2d1fdc19d7e
zachman9084 <42553338+zachman9084@users.noreply.github.com>
2018-08-23 08:56:29 -0500

    Add files via upload

---
+++ hello.C
@@ -0,0 +1,16 @@
+/**
+ * Author: Chris Bourke
+ * Date: 2016/11/02
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Zach Hoffman, Electrical Engineering\n");
+
+  return 0;
+}
